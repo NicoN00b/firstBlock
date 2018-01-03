511fbb2c12f35512530e7e383fa481708d8336b2
nr <nicholas.raethke@gmail.com>
2018-01-02 20:16:58 -0800

    create method for creating new blocks

--- firstBlock.js
+++ firstBlock.js
@@ -6,7 +6,7 @@ class Block{
   this.timestamp = timestamp;
   this.data = data;
   this.previousHash = previousHash;
-  this.hash = '';
+  this.hash = this.calculateHash();
   }
 
   calculateHash(){
@@ -20,6 +20,22 @@ class Blockchain{
   }
 
   createGenesisBlock(){
-    retunr new Block(0, "01/02/2018", "Genesis Block", "0");
+    return new Block(0, "02/01/2018", "Genesis Block", "0");
   }
+
+  getLatestBlock(){
+    return this.chain[this.chain.length -1];
+  }
+
+  addBlock(newBlock){
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock)
+  };
 }
+
+let nicoCoin = new Blockchain();
+nicoCoin.addBlock(new Block(1, "02/01/17", { amount: 4}));
+nicoCoin.addBlock(new Block(2, "04/01/17", { amount: 8}));
+
+console.log(JSON.stringify(nicoCoin, null, 4));

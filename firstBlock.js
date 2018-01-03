b94302a27c3a80d4f5612b97622fbba082c1e56c
nr <nicholas.raethke@gmail.com>
2018-01-02 19:52:26 -0800

    create genesis block.

--- firstBlock.js
+++ firstBlock.js
@@ -1,3 +1,4 @@
+const SHA256 = require('crypto-js/sha256');
 
 class Block{
   constructor(index, timestamp, data, previousHash = ''){
@@ -6,5 +7,19 @@ class Block{
   this.data = data;
   this.previousHash = previousHash;
   this.hash = '';
+  }
+
+  calculateHash(){
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+  }
+}
+
+class Blockchain{
+  constructor(){
+    this.chain = [this.createGenesisBlock()];
+  }
+
+  createGenesisBlock(){
+    retunr new Block(0, "01/02/2018", "Genesis Block", "0");
   }
 }
